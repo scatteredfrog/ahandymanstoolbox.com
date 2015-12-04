890096c5a7651454333c3fd106a902b2821f0340
seancourtney@fab4it.com <seancourtney@fab4it.com>
2015-12-04 12:27:33 -0600

    Update used tool display page

--- dog/models/Monkeywrench_model.php
+++ dog/models/Monkeywrench_model.php
@@ -5,7 +5,18 @@ class Monkeywrench_model extends CI_Model {
 
     public function getActiveTools() {
         $data['tool'] = array();
-        $this->db->select('id,name,category,purchase_price,public_notes,public_misc,picture_filename');
+        $ts = 'id,active,name,category,price_tag,craigslist_title,ebay_title,';
+        $ts .= 'purchase_date,purchase_price,buyers_premium,public_notes,public_notes,';
+        $ts .= 'purchased_from,item_details,public_notes,public_misc,';
+        $ts .= 'picture_filename,picture_filename1,picture_filename2,';
+        $ts .= 'picture_filename3,picture_filename4,picture_filename5,';
+        $ts .= 'picture_filename6,picture_filename7,picture_filename8,';
+        $ts .= 'picture_filename9,picture_filename10,picture_filename11,';
+        $ts .= 'picture_filename12,picture_filename13,picture_filename14,';
+        $ts .= 'picture_filename15,picture_filename16,picture_filename17,';
+        $ts .= 'picture_filename18,picture_filename19';
+        $this->db->select($ts);
+//        $this->db->select('id,name,category,price_tag,purchase_price,public_notes,public_misc,picture_filename');
         $this->db->from('tool_db');
         $this->db->where('active','1');
         $query = $this->db->get();
@@ -14,13 +25,76 @@ class Monkeywrench_model extends CI_Model {
         foreach($query->result() as $row) {
             $tool[$result_count]['id'] = $row->id;
             $tool[$result_count]['name'] = $row->name;
+            $tool[$result_count]['active'] = $row->active;
             $tool[$result_count]['category'] = $row->category;
+            $tool[$result_count]['craigslist_title'] = $row->craigslist_title;
+            $tool[$result_count]['ebay_title'] = $row->ebay_title;
+            $tool[$result_count]['purchase_date'] = $row->purchase_date;
             $tool[$result_count]['purchase_price'] = $row->purchase_price;
-            $tool[$result_count]['notes'] = $row->public_notes;
-            $tool[$result_count]['misc'] = $row->public_misc;
-            $tool[$result_count]['picture'] = $row->picture_filename;
+            $tool[$result_count]['public_notes'] = $row->public_notes;
+            $tool[$result_count]['public_misc'] = $row->public_misc;
+            $tool[$result_count]['picture_filename'] = $row->picture_filename;
+            $tool[$result_count]['picture_filename1'] = $row->picture_filename1;
+            $tool[$result_count]['picture_filename2'] = $row->picture_filename2;
+            $tool[$result_count]['picture_filename3'] = $row->picture_filename3;
+            $tool[$result_count]['picture_filename4'] = $row->picture_filename4;
+            $tool[$result_count]['picture_filename5'] = $row->picture_filename5;
+            $tool[$result_count]['picture_filename6'] = $row->picture_filename6;
+            $tool[$result_count]['picture_filename7'] = $row->picture_filename7;
+            $tool[$result_count]['picture_filename8'] = $row->picture_filename8;
+            $tool[$result_count]['picture_filename9'] = $row->picture_filename9;
+            $tool[$result_count]['picture_filename10'] = $row->picture_filename10;
+            $tool[$result_count]['picture_filename11'] = $row->picture_filename11;
+            $tool[$result_count]['picture_filename12'] = $row->picture_filename12;
+            $tool[$result_count]['picture_filename13'] = $row->picture_filename13;
+            $tool[$result_count]['picture_filename14'] = $row->picture_filename14;
+            $tool[$result_count]['picture_filename15'] = $row->picture_filename15;
+            $tool[$result_count]['picture_filename16'] = $row->picture_filename16;
+            $tool[$result_count]['picture_filename17'] = $row->picture_filename17;
+            $tool[$result_count]['picture_filename18'] = $row->picture_filename18;
+            $tool[$result_count]['picture_filename19'] = $row->picture_filename19;
+            $tool[$result_count]['price_tag'] = $row->price_tag;
             $result_count++;
         }
+        $this->db->select($ts);
+        $this->db->from('tool_db');
+        $this->db->where('active !=','1');
+        $query = $this->db->get();
+        foreach($query->result() as $row) {
+            $tool[$result_count]['id'] = $row->id;
+            $tool[$result_count]['name'] = $row->name;
+            $tool[$result_count]['active'] = $row->active;
+            $tool[$result_count]['category'] = $row->category;
+            $tool[$result_count]['craigslist_title'] = $row->craigslist_title;
+            $tool[$result_count]['ebay_title'] = $row->ebay_title;
+            $tool[$result_count]['purchase_date'] = $row->purchase_date;
+            $tool[$result_count]['purchase_price'] = $row->purchase_price;
+            $tool[$result_count]['public_notes'] = $row->public_notes;
+            $tool[$result_count]['public_misc'] = $row->public_misc;
+            $tool[$result_count]['picture_filename'] = $row->picture_filename;
+            $tool[$result_count]['picture_filename1'] = $row->picture_filename1;
+            $tool[$result_count]['picture_filename2'] = $row->picture_filename2;
+            $tool[$result_count]['picture_filename3'] = $row->picture_filename3;
+            $tool[$result_count]['picture_filename4'] = $row->picture_filename4;
+            $tool[$result_count]['picture_filename5'] = $row->picture_filename5;
+            $tool[$result_count]['picture_filename6'] = $row->picture_filename6;
+            $tool[$result_count]['picture_filename7'] = $row->picture_filename7;
+            $tool[$result_count]['picture_filename8'] = $row->picture_filename8;
+            $tool[$result_count]['picture_filename9'] = $row->picture_filename9;
+            $tool[$result_count]['picture_filename10'] = $row->picture_filename10;
+            $tool[$result_count]['picture_filename11'] = $row->picture_filename11;
+            $tool[$result_count]['picture_filename12'] = $row->picture_filename12;
+            $tool[$result_count]['picture_filename13'] = $row->picture_filename13;
+            $tool[$result_count]['picture_filename14'] = $row->picture_filename14;
+            $tool[$result_count]['picture_filename15'] = $row->picture_filename15;
+            $tool[$result_count]['picture_filename16'] = $row->picture_filename16;
+            $tool[$result_count]['picture_filename17'] = $row->picture_filename17;
+            $tool[$result_count]['picture_filename18'] = $row->picture_filename18;
+            $tool[$result_count]['picture_filename19'] = $row->picture_filename19;
+            $tool[$result_count]['price_tag'] = $row->price_tag;
+            $result_count++;
+        }
+        
         return $tool;
     }
     
--- dog/views/used_tools.php
+++ dog/views/used_tools.php
@@ -3,6 +3,30 @@
     echo "<script src='" . base_url('stuff/js/bootstrap.min.js') . "'></script>";
     echo link_tag('stuff/css/monkey.css');
 ?>
+    <nav class="navbar navbar-inverse">
+        <div class="container-fluid">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#hmtb_navbar" aria-expanded="false">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand toolboxBrand" href="#">&nbsp; &nbsp; A Handyman's Toolbox</a>
+            </div>
+            <div class="collapse navbar-collapse" id="hmtb_navbar">
+                <ul class="tb-menu-items nav navbar-nav navbar-right">
+                    <li><a href="/">Home</a></li>
+                    <li><a href="#">About Me</a></li>
+                    <li><a href="#">My Collection</a></li>
+                    <li><a href="#">New Tools for the Homeowner</a></li>
+                    <li class="active"><a href="#">Used Tools</a></li>
+                    <li><a href="#">Toolbox Registry</a></li>
+                </ul>
+            </div>
+        </div>
+    </nav>
 <div id="used_tool_carousel" class="carousel slide" data-ride="carousel">
     <!-- indicators -->
     <ol class="carousel-indicators">
@@ -38,11 +62,13 @@
         }
         $item .= '">';
         // IMAGE
-        $item .= '<img src="/stuff/images/tool_photos/' . $tool['picture'] . '" alt="..." class="center-block"';
-        $item .= 'data-price="' . $tool['purchase_price'] . '" ';
+        $item .= '<img src="/stuff/images/tool_photos/' . $tool['picture_filename'] . '" alt="..." class="center-block"';
+        $item .= 'data-price="' . $tool['price_tag'] . '" ';
         $item .= 'data-category="' . $tool['category'] . '" ';
-        $item .= 'data-notes="' . htmlentities($tool['notes']) . '" ';
-        $item .= 'data-additional="' . $tool['misc'] . '" ';
+        $item .= 'data-notes="' . htmlentities($tool['public_notes']) . '" ';
+        $item .= 'data-additional="' . $tool['public_misc'] . '" ';
+        $item .= 'data-mainpicture="' . $tool['picture_filename'] . '" ';
+        $item .= 'data-active="' . $tool['active'] . '" ';
         $item .= '/>';
         // CAPTION
         $item .= '<div class="carousel-caption">';
@@ -66,31 +92,46 @@
         <span class="glyphicon glyphicon-chevron-right"></span>
     </a>
 </div>
+<div class="chunk">
 <div class="row">
+    <div class="row largeFile">
+        <div class="col-xs-1 col-md-1 col-lg-3 col-sm-1"></div>
+        <div class="col-xs-5 col-md-3 col-sm-3 col-lg-2">Full size picture:</div>
+        <div class="col-xs-6 col-md-6 col-sm-6 col-lg-4 toolFullSize"></div>
+        <div class="col-md-2 col-lg-3 col-sm-2"></div>
+    </div>
     <div class="row" id="category_row">
         <div class="col-xs-1 col-md-1 col-lg-3 col-sm-1"></div>
         <div class="col-xs-5 col-md-3 col-sm-3 col-lg-2">Category:</div>
         <div class="col-xs-6 col-md-6 col-sm-6 col-lg-4 toolCategory"></div>
         <div class="col-md-2 col-lg-3 col-sm-2"></div>
     </div>
-    <div class="row">
+    <div class="row priceRow">
         <div class="col-xs-1 col-md-1 col-lg-3 col-sm-1"></div>
         <div class="col-xs-5 col-md-3 col-sm-3 col-lg-2">Price:</div>
         <div class="col-xs-6 col-md-6 col-sm-6 col-lg-4 toolPrice"></div>
         <div class="col-md-2 col-lg-3 col-sm-2"></div>
     </div>
-    <div class="row">
+    <div class="row notesRow">
         <div class="col-xs-1 col-md-1 col-lg-3 col-sm-1"></div>
         <div class="col-xs-5 col-md-3 col-sm-3 col-lg-2">Notes:</div>
         <div class="col-xs-6 col-md-6 col-sm-6 col-lg-4 toolNotes"></div>
         <div class="col-md-2 col-lg-3 col-sm-2"></div>
     </div>
-    <div class="row">
+    <div class="row additionalInfoRow">
         <div class="col-xs-1 col-md-1 col-lg-3 col-sm-1"></div>
         <div class="col-xs-5 col-md-3 col-sm-3 col-lg-2">Additional info:</div>
         <div class="col-xs-6 col-md-6 col-sm-6 col-lg-4 toolAdditional"></div>
         <div class="col-md-2 col-lg-3 col-sm-2"></div>
     </div>
+    <div class="row">&nbsp;</div>
+    <div class="row availabilityRow">
+        <div class="col-xs-1 col-md-1 col-lg-3 col-sm-1"></div>
+        <div class="col-xs-5 col-md-3 col-sm-3 col-lg-2">Availability:</div>
+        <div class="col-xs-6 col-md-6 col-sm-6 col-lg-4 toolAvailability"></div>
+        <div class="col-md-2 col-lg-3 col-sm-2"></div>
+    </div>
+</div>
 </div>
 <script>
     $('.carousel').carousel({
@@ -100,17 +141,58 @@
     updateToolData();
     
     $('#used_tool_carousel').on('slid.bs.carousel', function() {
+//        $('.chunk').hide();
         updateToolData();
+//        $('.chunk').show();
     });
     
     function updateToolData() {
-        $('.toolNotes').html($('.active img').data('notes'));
-        $('.toolPrice').html('$'+$('.active img').data('price'));
-        if ( $('.active img').data('category')) {
-            $.post('parseCategory', { 'category': $('.active img').data('category') }, function(data) {
+        var mainPicture = $('.active img').data('mainpicture');
+        var notAvail = "Sorry, this tool is not available at this time.";
+        var isAvail = "<span class='toolAvailable'>AVAILABLE TO BUY. Click here if interested.</span>";
+        var actIm = $('.active img');
+        
+        $('.largeFile').css('display','none');
+        if (actIm.data('notes')) {
+            $('.toolNotes').html(actIm.data('notes'));
+            $('.notesRow').show();
+        } else {
+            $('.notesRow').hide();
+        }
+        
+        if (actIm.data('additional_info')) {
+            $('.toolAdditional').html(actIm.data('additional_info'));
+            $('.additionalInfoRow').show();
+        } else {
+            $('.additionalInfoRow').hide();
+        }
+
+    if ( actIm.data('category')) {
+            $.post('parseCategory', { 'category': actIm.data('category') }, function(data) {
                 $('.toolCategory').html(data);
                 $('#category_row').css('display','block');
             });
+        } else {
+            $('#category_row').css('display','block');
+        }
+                    
+        if (mainPicture.indexOf('_resized') > 0) {
+            var originalSize = mainPicture.replace('_resized','');
+            var img_src = '/stuff/images/tool_photos/' + originalSize;
+            $('.toolFullSize').html('<a href="'+img_src+'" target="_blank">Click here</a>');
+            $('.largeFile').css('display','block');
+        }
+        
+        if (actIm.data('active') && actIm.data('price')) {
+            $('.toolPrice').html('$'+actIm.data('price'));
+            $('.priceRow').show();
+            $('.toolAvailability').html(isAvail);
+        } else  if (actIm.data('active')) {
+            $('.priceRow').hide();
+            $('.toolAvailability').html(isAvail);
+        } else {
+            $('.priceRow').hide();
+            $('.toolAvailability').html(notAvail);
         }
     }
 </script>
